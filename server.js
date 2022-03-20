89bd428456dff16302c3ace3f07bd89feef868fa
renatogm24 <renato.garay@pucp.pe>
2022-03-20 18:27:58 -0500

    Update server

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 const AllMyAuthorsRoutes = require("./server/routes/author.routes");
 AllMyAuthorsRoutes(app);
 
-app.listen(8000, () => console.log("The server is all fired up on port 8000"));
+app.listen(8000, () => console.log("The server is all fired up, on port 8000"));
